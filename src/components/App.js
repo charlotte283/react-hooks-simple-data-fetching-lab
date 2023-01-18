433de1b87ff124b7d19fcb8c430cc306ae9ac314
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2023-01-18 14:03:06 +0300

    char

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,26 @@
 // create your App component here
+import React, { useEffect, useState } from "react";
+
+
+const App = () => {
+
+    const [dogLink, setDogLink] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(response => response.json())
+        .then(data => {
+            setDogLink(data.message)
+            setIsLoading(false)
+        })
+    }, [])
+
+    return (
+        <div>
+            {isLoading ? <p>Loading...</p> : <img src={dogLink} alt={"A Random Dog"} />}
+        </div>
+    );}
+
+
+export default App;
